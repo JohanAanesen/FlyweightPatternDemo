d78ab29fd97559bce0f9f3c186929c0966aafacf
Johan Aanesen <johanaan@stud.ntnu.no>
2018-10-02 23:48:23 +0200

    Commented and re-arranged code for readability

--- src/Flyweight/CircleFactory.java
+++ src/Flyweight/CircleFactory.java
@@ -7,19 +7,25 @@ import java.util.HashMap;
 
 public class CircleFactory {
 
-    public static final HashMap<Color, Circle> colorHashMap = new HashMap<>();
+    public static final HashMap<Color, Circle> colorHashMap = new HashMap<>(); //Hashmap holds previous Circles
 
+    /**
+     * Title: getCircle
+     * Desc: Returns circle object
+     * @param color Color of the circle
+     * @return Circle
+     */
     public static Circle getCircle(Color color){
 
-        Circle circle = (Circle)colorHashMap.get(color);
+        Circle circle = (Circle)colorHashMap.get(color); //Try and fetch circle with desired color
 
-        if (circle == null){
-            circle = new Circle();
-            circle.setFill(color);
+        if (circle == null){        //Circle with desired color does not exist
+            circle = new Circle();  //Creates New Circle
+            circle.setFill(color);  //Sets color
 
-            colorHashMap.put(color, circle);
+            colorHashMap.put(color, circle); //Adds the new Circle with the new color to the map
         }
 
-        return circle;
+        return circle;              //Return Circle
     }
 }
--- src/NoFlyweight/Controller.java
+++ src/NoFlyweight/Controller.java
@@ -30,28 +30,33 @@ public class Controller {
         colors[3] = Color.GREENYELLOW;
         colors[4] = Color.HOTPINK;
 
+        //TIME AND MEMORY LOG START
         var time1 = System.currentTimeMillis();
         MemoryUsage heapMemoryUsage1 = ManagementFactory.getMemoryMXBean().getHeapMemoryUsage();
         var mem1 = heapMemoryUsage1.getUsed();
 
         for (int i = 0; i < 100000; i++) {
-            Color color = colors[rand.nextInt(5)];
-            circles[i] = new Circle(); //Ny sirkel til hvert objekt
+            Color color = colors[rand.nextInt(5)];  //Random Color
 
-            circles[i].setFill(color);
-            circles[i].setCenterX(rand.nextDouble()*600);
-            circles[i].setCenterY(rand.nextDouble()*600);
-            circles[i].setRadius(rand.nextDouble()*25);
+            circles[i] = new Circle();                      //New Circle
+            circles[i].setFill(color);                      //Set color
 
+            circles[i].setCenterX(rand.nextDouble()*600);   //Random location x
+            circles[i].setCenterY(rand.nextDouble()*600);   //Random location y
+            circles[i].setRadius(rand.nextDouble()*25);     //Random radius
+
+            //DRAWING CIRCLE TO CANVAS
             gc.strokeOval(circles[i].getCenterX(), circles[i].getCenterY(), circles[i].getRadius(), circles[i].getRadius());
             gc.setFill(circles[i].getFill());
             gc.fillOval(circles[i].getCenterX(), circles[i].getCenterY(), circles[i].getRadius(), circles[i].getRadius());
         }
 
+        //TIME AND MEMORY LOG END
         var time2 = System.currentTimeMillis();
         MemoryUsage heapMemoryUsage2 = ManagementFactory.getMemoryMXBean().getHeapMemoryUsage();
         var mem2 = heapMemoryUsage2.getUsed();
 
+        //Printing results
         System.out.println("Time: "+(time2-time1) + " ms");
         System.out.println("Memory: "+(mem2-mem1)/1000000 + " MB");
     }
